3014bf101764d8bd4682c3b9f0e5bdf177095dc4
Marakas <igorpetriv.bsc@gmail.com>
2025-03-28 22:35:37 +0200

    Gallery added

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { createMarkup, errorMessage } from './render-functions';
+export function getdata(searchValue) {
+    
+    const myApiKey = '49580099-ba49dcf3c416d0b66883e5025'; 
+    axios.get('https://pixabay.com/api/', {
+        params: {
+            key: `${myApiKey}`,
+            q: `${searchValue}`,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: 200,
+            
+            
+        }
+    })
+        .then(response => {
+            if (response.data.hits.length === 0) {
+                errorMessage(`Sorry, there are no images matching ${searchValue}. Please try again!`);
+                return;
+            }
+
+            createMarkup(response.data.hits);
+        })
+        
+        .catch(error => {
+            console.error(error);
+            errorMessage('Something went wrong. Please try again later.');
+        })
+    
+
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,115 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { getdata } from "./pixabay-api";
+
+
+
+// !-----------FUNCTION ADDING CARDS WITH IMAGES TO GALLERY-------------
+export function createMarkup(data) {
+    const gallery = document.querySelector('.gallery');
+    
+    //ADD PHOTOS TO GALLARY
+    const galleryItemsList = data
+        .map(data => {
+            const { likes,
+                    views,
+                    downloads,
+                    comments,
+                    tags,
+                    largeImageURL,
+                    webformatURL   
+                } = data;
+            
+            return `<li class="img-card">
+                        <a class="gallery-link" href= ${largeImageURL}>
+                            <img
+                            class="gallery-image"
+                            src= ${webformatURL}
+                            alt= ${tags}
+                            />
+                        </a>
+                        <ul class="img-info">
+                            <li class="info">
+                                <svg class="icon-info" width="18" height="18">
+                                    <use href="./public/sprite.svg#icon-like"></use>
+                                </svg>
+                                <p>${likes}</p>
+                            </li>
+                            <li class="info">
+                                <svg class="icon-info" width="18" height="18">
+                                    <use href="./public/sprite.svg#icon-eye"></use>
+                                </svg>
+                                <p>${views}</p>
+                            </li>
+                            <li class="info">
+                                <svg class="icon-info" width="18" height="18">
+                                    <use href="./public/sprite.svg#icon-dialog"></use>
+                                </svg>
+                                <p>${comments}</p>
+                            </li>
+                            <li class="info">
+                                <svg class="icon-info" width="18" height="18">
+                                    <use href="./public/sprite.svg#icon-gallery-download"></use>
+                                </svg>
+                                <p>${downloads}</p>
+                            </li>
+                        </ul>
+                    </li>`
+        })
+        .join('');
+    gallery.innerHTML = '';
+    gallery.insertAdjacentHTML('afterbegin', galleryItemsList);
+    
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+    
+    lightbox.refresh();
+}
+
+
+export function handleSearch() {
+    const searchBar = document.querySelector('.form');
+    const icon = document.querySelector('.icon-img');
+
+    
+    searchBar.addEventListener('input', event => {
+            let searchValue = event.target.value.trim();
+        if (searchValue) {
+            icon.style.fill = '#1875FF';
+        }
+        else {
+            icon.style.fill = '';
+        }
+        })
+    
+    searchBar.addEventListener('submit', event => {
+        event.preventDefault();
+        let searchValue = event.target['search-text'].value.trim();
+        if (event.target.name === 'search' && !searchValue) {
+            errorMessage(`Sorry`);
+            return;
+        }
+
+        getdata(searchValue);
+    })
+
+}
+
+
+
+export function errorMessage(message) {
+ iziToast.error({
+   message: message,
+   position: window.innerWidth <= 768 ? 'bottomCenter' : 'topCenter',
+   transitionIn: 'fadeInDown',
+   backgroundColor: 'rgb(231, 19, 36)',
+   messageColor: 'white',
+   iconColor: 'white',
+   messageSize: '16px',
+   class: 'toast',
+});
+}
